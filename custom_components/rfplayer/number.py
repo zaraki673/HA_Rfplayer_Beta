aa838dc842ee2c4b58efbc12b3596904378dc3dd
Doubledom45 <97252459+Doubledom45@users.noreply.github.com>
2022-09-15 11:25:52 +0100

    Update number.py
    Mise à jour pour  Home Assistant Core 2022.10, "Entity None (<class 'custom_components.rfplayer.number.RfplayerJammingNumber'>) is using deprecated NumberEntity features"

--- custom_components/rfplayer/number.py
+++ custom_components/rfplayer/number.py
@@ -22,10 +22,10 @@ class RfplayerJammingNumber(RfplayerDevice, NumberEntity):
 
     def __init__(self):
         """Init the number rfplayer entity."""
-        self._attr_min_value = 0
-        self._attr_max_value = 10
-        self._attr_mode = "slider"
-        self._attr_entity_category = EntityCategory.CONFIG
+        self._attr_native_min_value = 0
+        self._attr_native_max_value = 10
+        self._attr_native_mode = "slider"
+        self._attr_native_entity_category = EntityCategory.CONFIG
         super().__init__("JAMMING", device_id=0, name="Jamming detection level")
 
     async def async_added_to_hass(self):
